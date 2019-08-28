fa98a2e81e7ea60bcea3a2de69af40251ef95f09
Alex K <zexa@cock.li>
2019-08-28 13:18:38 +0300

    Added big break

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -2,6 +2,9 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 
+let iteration = 0;
+let state = 'rest';
+
 const blinkBackground = function(color, times = 3) {
 	let timeCurrent = 0;
 	const blinkWhite = function() {
@@ -21,14 +24,29 @@ const blinkBackground = function(color, times = 3) {
 	blink();
 }
 
-const setWorkTime = function() {
+const setWorkTime = function(time = minute * 25) {
+	state = 'work';
 	blinkBackground('#a63c6b');
-	setTimeout(setRestTime, minute * 25);
+	setTimeout(toggleState, time);
 }
 
-const setRestTime = function() {
+const setRestTime = function(time = minute * 5) {
+	state = 'rest';
 	blinkBackground('#3c76a6');
-	setTimeout(setWorkTime, minute * 5);
+	iteration++;
+	setTimeout(toggleState, time);
+}
+
+const toggleState = function() {
+	if (state === 'rest') {
+		setWorkTime();
+	} else if (state === 'work') {
+		if (iteration > 3) {
+			setRestTime(minute * 30);
+		} else {
+			setRestTime();
+		}
+	}
 }
 
 setWorkTime();
