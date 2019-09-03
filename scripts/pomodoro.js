9a792df8ccbb0f75f12fbce2f1f20beff21d3c12
Alex K <zexa@cock.li>
2019-09-03 13:32:29 +0300

    Added config constants

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -2,9 +2,18 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 
+// Configs
+
 const defaultWorkTime = minute * 25;
 const defaultRestTime = minute * 5;
 const defaultBigRestTime = minute * 30;
+
+const blinkColor = '#ffffff';
+const workColor = '#ff4242';
+const restColor = '#3c76a6';
+const bigRestColor = '#3ca661';
+
+const iterationsBeforeBigRest = 3;
 
 let iteration = 0;
 let state = 'rest';
@@ -12,7 +21,7 @@ let state = 'rest';
 const blinkBackground = function(color, times = 3) {
 	let timeCurrent = 0;
 	const blinkWhite = function() {
-		document.body.style.background = '#FFFFFF';
+		document.body.style.background = blinkColor;
 		setTimeout(blinkColor, 500);
 	}
 	const blinkColor = function() {
@@ -30,20 +39,20 @@ const blinkBackground = function(color, times = 3) {
 
 const setWorkTime = function(time = defaultWorkTime) {
 	state = 'work';
-	blinkBackground('#a63c6b');
+	blinkBackground(workColor);
 	setTimeout(toggleState, time);
 }
 
 const setRestTime = function(time = defaultRestTime) {
 	state = 'rest';
-	blinkBackground('#3c76a6');
+	blinkBackground(restColor);
 	iteration++;
 	setTimeout(toggleState, time);
 }
 
 const setBigRestTime = function(time = defaultBigRestTime) {
 	state = 'big rest';
-	blinkBackground('#3ca661');
+	blinkBackground(bigRestColor);
 	setTimeout(toggleState, time);
 }
 
@@ -51,7 +60,7 @@ const toggleState = function() {
 	if ((state === 'rest') || (state === 'big rest')) {
 		setWorkTime();
 	} else if (state === 'work') {
-		if (iteration > 3) {
+		if (iteration > iterationsBeforeBigRest) {
 			setBigRestTime();
 			iteration = 0;
 		} else {
